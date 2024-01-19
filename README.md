960caa035a79c0c0c8bf896f860f5056a74ab7a0
Jesus Cerquides <j.cerquides@csic.es>
2024-01-19 13:20:27 +0100

    Added generation of problems

--- README.md
+++ README.md
@@ -8,10 +8,18 @@ Currently only Linux is supported (Windows users can use WSL)
 
 1. Clone the repo
 2. source bin/init-local.sh
+3. You can launch jupyter from console.
 
 ## Directories
 
 * src: keeps the python sources
 * bin: executables
 * nb: Jupyter notebooks
+* exp: Stores the experiments
+
+## Coding and git usage etiquette
+
+* Avoid storing large files or a large number of files in git. 
+* Use `jupytext` to pair your notebooks (the repo is configured so that any notebook in the `nb` directory is automatically paired) and commit only the .py files. No .ipynb file should be in the repo.
+
 
---
+++ nb/Generate 3sat problems.py
@@ -0,0 +1,57 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from generation import generate_3sat
+import random
+import numpy as np
+from pathlib import Path
+
+# %%
+# Generate problems for Fig.1 in Nusslein
+
+random.seed(1345)
+n_vars = np.arange(20,401,20)
+dir = "../exp/e1/problems"
+p_dir = Path(dir)
+p_dir.mkdir(parents=True, exist_ok=True)
+for vars in n_vars:
+    p = generate_3sat(vars, ratio=4.2)
+    with open(p_dir / ("p"+str(vars)+".cnf"),"w") as f:
+        f.write(p)
+
+        
+    
+    
+    
+    
+
+# %%
+# Generate problems for Fig.2 in Nusslein
+
+
+random.seed(13435)
+n_vars = np.arange(15,28,3)
+num_instances = 20
+dir = "../exp/e2/problems"
+p_dir = Path(dir)
+p_dir.mkdir(parents=True, exist_ok=True)
+for vars in n_vars:
+    for i in range(num_instances):
+        p = generate_3sat(vars, ratio=4.2)
+        with open(p_dir / ("p"+str(vars)+"-"+str(i)+".cnf"),"w") as f:
+            f.write(p)
+
+
+# %%
